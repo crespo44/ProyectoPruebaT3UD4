f64d9db1eabfeed3ad2de5cb3005347938827bf5
jorge <jorge@JORGE.home>
2022-03-17 21:51:06 +0100

    Proyecto Java sesión 2

--- UD4 Tarea 3 Repositorio GITHUB/src/Repositorio.java
+++ UD4 Tarea 3 Repositorio GITHUB/src/Repositorio.java
@@ -3,6 +3,7 @@ public class Repositorio {
 
 	public static void main(String[] args) {
 		System.out.println("En este proyecto trabajamos con el repositorio GITHUB");
+		System.out.println("Este es para la segunda versión");
 	}
 
 }
